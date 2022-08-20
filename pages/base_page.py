39ed6bd5ed9280ff930d24d7c4117dd6ab003081
Dmitry M <a1a1a123@mail.ru>
2022-08-20 18:59:34 +0500

    moved checks from main_page to base_page. inherited methods from base_page to main_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException  # в начале файла
 import math
+from .locators import BasePageLocators
 
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -21,6 +22,17 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+        # alert = self.browser.switch_to.alert #если есть алерт при нажатии
+        # alert.accept()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(
+            *BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
