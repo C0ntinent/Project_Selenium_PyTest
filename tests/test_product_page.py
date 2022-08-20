423a70e50ad246403db459f6b08a95b988f9f545
Dmitry M <a1a1a123@mail.ru>
2022-08-20 19:04:08 +0500

    added tests for product_page: checking login link; moving from product_page to login_page

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,3 +1,7 @@
+import time
+
+from pages.login_page import LoginPage
+from pages.main_page import MainPage
 from pages.product_page import ProductPage
 
 
@@ -6,3 +10,19 @@ def test_guest_should_see_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_product_url()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
