567ff4fe8e8704cf69c19c52fc74f43ed3eacbfd
Dmitry M <a1a1a123@mail.ru>
2022-08-22 12:53:21 +0500

    added checking basket page and authorized_user

--- pages/base_page.py
+++ pages/base_page.py
@@ -34,6 +34,19 @@ class BasePage:
         assert self.is_element_present(
             *BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(
+            *BasePageLocators.USER_ICON), "User icon is not presented," \
+                                          " probably unauthorised user"
+
+    def go_to_basket_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
+        login_link.click()
+
+    def should_be_basket_link(self):
+        assert self.is_element_present(
+            *BasePageLocators.BASKET_LINK), "Basket link is not presented"
+
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
